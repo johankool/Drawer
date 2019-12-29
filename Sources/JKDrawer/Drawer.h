830cf27c2cea5c4df0c84525b1a455aa333c4827
Johan Kool <johankool@users.noreply.github.com>
2019-12-29 11:41:03 +0100

    Fix build

---
+++ Sources/JKDrawer/Drawer.h
@@ -0,0 +1,19 @@
+//
+//  Drawer.h
+//  Drawer
+//
+//  Created by Johan Kool on 10/11/2018.
+//  Copyright © 2018 Johan Kool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Drawer.
+FOUNDATION_EXPORT double DrawerVersionNumber;
+
+//! Project version string for Drawer.
+FOUNDATION_EXPORT const unsigned char DrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Drawer/PublicHeader.h>
+
+
